edd82fd3b5d53689ab85723b04f8a81cad95993f
Osama Assaf <96818454+OsamaAssaf@users.noreply.github.com>
2024-07-29 09:05:12 +0300

    commit

--- app/src/main/java/com/assaf/androidcallapi/repositories/NetworkService.java
+++ app/src/main/java/com/assaf/androidcallapi/repositories/NetworkService.java
@@ -1,15 +1,15 @@
 package com.assaf.androidcallapi.repositories;
 
-import android.content.Context;
-
 import androidx.annotation.NonNull;
 
 import java.io.IOException;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class NetworkService {
@@ -40,5 +40,33 @@ public class NetworkService {
         });
     }
 
+    public static void post(String url, NetworkCallback callback) {
+
+        final RequestBody requestBody = new FormBody.Builder()
+//                .add("title", "New Title")
+                .build();
+
+        final Request postRequest = new Request.Builder()
+                .url(url)
+                .post(requestBody)
+                .build();
+
+        client.newCall(postRequest).enqueue(new Callback() {
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful() && response.body() != null) {
+                    callback.onSuccess(response);
+                } else {
+                    callback.onFailure("Response not successful or body is null");
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                System.out.println("onFailure: " + e.getMessage());
+                callback.onFailure("Response not successful or body is null");
+            }
+        });
+    }
 
 }
